27053a055cd763ce0e879ad5d79ddcfe19ae0b87
Victor Samuel dos Santos <victor.samuel1@aluno.ifsp.edu.br>
2024-08-07 21:31:00 -0300

    Update predict_image.py

--- scripts/predict_image.py
+++ scripts/predict_image.py
@@ -15,8 +15,6 @@
 #     img_path = 'path/to/test_image.jpg'
 #     predicted_class = predict_image(img_path)    print(f'Predicted Class: {predicted_class}')
 
-
-
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
@@ -25,31 +23,24 @@ import os
 # Carregando o modelo
 model = load_model('models/plant_disease_model.h5')
 
-
 def predict_image(img_path):
-    
     img = image.load_img(img_path, target_size=(256, 256))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0) / 255.0
-    
     # previsao
     prediction = model.predict(img_array)
     class_idx = np.argmax(prediction, axis=1)
-    
     # indices para classes
     class_labels = {0: 'Strawberry__Healthy', 1: 'Strawberry__Leaf_scorch'}
     return class_labels[class_idx[0]]
 
-
 if __name__ == "__main__":
-
     # imagem para teste
     test_image_path = 'data\plantvillage\Strawberry___Leaf_scorch\0a08af15-adfe-447c-8ed4-17ed2702d810___RS_L.Scorch 0054.JPG'  
     
     
     if not os.path.exists(test_image_path):
-        raise FileNotFoundError(f"A imagem de teste não foi encontrada: {test_image_path}")
-    
+        raise FileNotFoundError(f"A imagem de teste não foi encontrada: {test_image_path}") 
     # fazendo a previsao com a imagem de teste
     predicted_class = predict_image(test_image_path)
     print(f'Predicted Class: {predicted_class}')
